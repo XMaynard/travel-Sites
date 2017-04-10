70713f7d1104bf7aba200a300459830fc56eca77
BigO <course@africalendar.com>
2017-04-10 17:10:10 -0600

    completed modal overlay

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
-import smoothScroll from 'jquery-smooth-scrol';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor(){
